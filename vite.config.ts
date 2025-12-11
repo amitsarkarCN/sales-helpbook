3492700837c7b59e4f8ca3a9a57d05fe49143ad5
Amit Sarkar <amitskr6@gmail.com>
2025-12-11 17:09:31 +0530

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
+    base: process.env.VITE_BASE_PATH || "/sales-helpbook"
     const env = loadEnv(mode, '.', '');
     return {
       server: {
