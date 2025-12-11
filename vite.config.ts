a13c29876f16eb061a8817876234e3194a744188
amitsarkarCN <amit.sarkar@codingninjas.com>
2025-12-11 16:51:15 +0530

    feat: Refactor dependencies and UI elements
    Removes unused development dependencies and updates CDN links for modules. Improves the aesthetic and functional presentation of alumni data on the detail page by consolidating year and CTC into a single styled component.

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    base: process.env.VITE_BASE_PATH || "/sales-helpbook"
     const env = loadEnv(mode, '.', '');
     return {
       server: {
